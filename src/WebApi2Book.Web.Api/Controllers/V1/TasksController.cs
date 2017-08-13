54a0137728dcd5d6e77cb621a38a5ee55e1e525e
SatishPrajapati <satishprajapati96@gmail.com>
2017-08-13 13:21:45 +0530

    versioning of controllers

---
+++ src/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi2Book.Web.Api.Models;
+using WebApi2Book.Web.Common.Routing;
+
+namespace WebApi2Book.Web.Api.Controllers.V1
+{
+    [ApiVersion1RoutePrefix("tasks")]
+    public class TasksController : ApiController
+    {
+        [HttpPost][Route("", Name ="AddTaskRoute")]
+        public Task AddTask(HttpRequestMessage requestMessage, Task newTask)
+        {
+            return new Task
+            {
+                Subject = "In V1. New task subject = " + newTask.Subject
+            };
+        }
+    }
+}
